1055349d8d1458c47753d77605875b7311b891d0
AgosGarcia <agosgarcias@gmail.com>
2021-04-12 21:49:41 -0300

    creacion de objetos

--- js/app.js
+++ js/app.js
@@ -9,3 +9,17 @@ console.log(persona1.password);
 
 persona1.validar();
 
+const categoria1 = []
+
+categoria1.push(new Pelotas(1, babolat, 3, 150));
+categoria1.push(new Pelotas(2, babolat, 6, 250));
+categoria1.push(new Pelotas(3, wilson, 3, 180));
+categoria1.push(new Pelotas(4, dunlop, 3, 200));
+
+const categoria2 = []
+
+categoria2.push(new Raquetas(1, babolat, 17.5, 3450));
+categoria2.push(new Raquetas(2, babolat, 21, 4330));
+categoria2.push(new Raquetas(3, wilson, 27, 7200));
+categoria2.push(new Raquetas(4, price, 25, 7800));
+categoria2.push(new Raquetas(5, wilson, 23.5, 6450));
--- js/funcion.js
+++ js/funcion.js
@@ -12,3 +12,30 @@ class Perfil {
         console.log(this.nombre + " " + this.apellido + " " + "esta logueado");
     }
 }
+
+class Pelotas {
+    constructor(id, marca, unidades, precio){
+        this.id = parseInt(id);
+        this.marca = marca.toString;
+        this.unidades = parseInt(unidades);
+        this.precio = parseFloat(precio);
+        this.tubo = true; 
+    }
+
+    validar (){
+        console.log(this.nombre + " " + this.apellido + " " + "esta logueado");
+    }
+}
+class Raquetas {
+    constructor(id, marca, pulgadas, precio){
+        this.id = parseInt(id);
+        this.marca = marca.toString;
+        this.tpulgadas = parseFloat(pulgadas);
+        this.precio = parseFloat(precio);
+        this.color = color.toString;
+    }
+
+    validar (){
+        console.log(this.nombre + " " + this.apellido + " " + "esta logueado");
+    }
+}
